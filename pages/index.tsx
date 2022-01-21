727f6d7671e7a461356092efb7f8d16811a3499a
Sangho Moon <shmoon2917@jirocorp.io>
2022-01-21 22:51:54 +0900

    refactor: 사용하지 않는 코드 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,11 @@
 import React from 'react';
-import { GetStaticProps } from 'next';
-import EventList from '../components/events/event-list';
-import { Event, getFeaturedEvents } from '../helpers/api-utils';
-import Head from 'next/head';
 
-interface Props {
-  events: Event[];
-}
-
-const HomePage: React.FC<Props> = ({ events }) => {
+const HomePage: React.FC = () => {
   return (
     <div>
-      <Head>
-        <title>Next.js Events</title>
-        <meta
-          name='description'
-          content='Find a lot of great events that allow to you evolve'
-        />
-      </Head>
-      <EventList items={events} />
+      <h1>The Home Page</h1>
     </div>
   );
 };
 
 export default HomePage;
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents,
-    },
-    revalidate: 1800,
-  };
-};
